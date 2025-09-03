5a23ce3dc9a977ed2f6eacf767db46365d88706d
Hrishi Shah <114273264+Hrishirt@users.noreply.github.com>
2025-09-02 21:38:43 -0600

    Updated calculator

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
 console.log("JavaScript file loaded!");
 const numberButtons = document.querySelectorAll(".numberButton");
-console.log("Button found:", oneButton);
 
 numberButtons.forEach(button => {
     button.addEventListener("click", addNumToForm)
 });
 
+firstNumber = ''
+secondNumber = ''
 // oneButton.addEventListener("click", addOneToForm);
 const screen = document.querySelector("#result");
 console.log("Screen found:", screen);
 
+const equal = document.querySelector("#equals");
+equal.addEventListener("click", equalSign)
 const addButton = document.getElementById("addOperator");
 console.log("Add Button found: ", addButton)
+
+addButton.addEventListener("click", addOperation)
 
 function addNumToForm(event){
     let val = event.target.textContent || event.target.innerText
@@ -25,16 +30,15 @@ function clearScreen() {
     screen.value= ''
 }
 
-/*
+
 function addOperation() {
+    firstNumber = screen.value
+    secondNumber = screen.value
+    console.log(firstNumber);
     let val = addButton.textContent || addButton.innerText
-    if (Number.isInteger(screen.value)){
-        console.log(screen.value + " is an integer")
-        screen.value = val
-    }
-    else {
-        console.log(screen.value + " is not a integer")
-    }
-    
+    screen.value += val
 }
-    */
+
+function equalSign() {
+    screen.value = parseInt(firstNumber) + parseInt(secondNumber);
+}
